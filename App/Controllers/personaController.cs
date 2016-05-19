4cd2e05cde67f71729d00a03ab1b5bca0e4ddb9b
RichardLaptop <r_zapatac@hotmail.com>
2016-05-18 23:26:08 -0500

    base
    base

---
+++ App/Controllers/personaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using App.Models;
+using BE;
+
+namespace App.Controllers
+{
+    public class personaController : Controller
+    {
+        private AppContext db = new AppContext();
+
+        // GET: persona
+        public ActionResult Index()
+        {
+            return View(db.personas.ToList());
+        }
+
+        // GET: persona/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: persona/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: persona/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonaId,Nombres,Paterno,Materno,NombreCompleto,DNI,Celular,Correo,Sexo,FechaNacimiento")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.personas.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: persona/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: persona/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonaId,Nombres,Paterno,Materno,NombreCompleto,DNI,Celular,Correo,Sexo,FechaNacimiento")] persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: persona/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            persona persona = db.personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: persona/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            persona persona = db.personas.Find(id);
+            db.personas.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ App/Controllers/rolController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using App.Models;
+using BE;
+
+namespace App.Controllers
+{
+    public class rolController : Controller
+    {
+        private AppContext db = new AppContext();
+
+        // GET: rol
+        public ActionResult Index()
+        {
+            return View(db.rols.ToList());
+        }
+
+        // GET: rol/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            rol rol = db.rols.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // GET: rol/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: rol/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RolId,Denominacion")] rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.rols.Add(rol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rol);
+        }
+
+        // GET: rol/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            rol rol = db.rols.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: rol/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RolId,Denominacion")] rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rol).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rol);
+        }
+
+        // GET: rol/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            rol rol = db.rols.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: rol/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            rol rol = db.rols.Find(id);
+            db.rols.Remove(rol);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
