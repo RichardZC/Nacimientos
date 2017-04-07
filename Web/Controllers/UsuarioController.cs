418cacc132aa3643e7b425767955203693274121
RichardLaptop <r_zapatac@hotmail.com>
2017-04-07 15:45:44 -0500

    parcial

--- Web/Controllers/UsuarioController.cs
+++ Web/Controllers/UsuarioController.cs
@@ -19,5 +19,13 @@ namespace Web.Controllers
         {
             return Json(UsuarioBL.Listar(),JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult parcial()
+        {
+            UsuarioBL.ActualizarParcial(new BE.usuario { UsuarioId = 1,Nombre="xxxx", Clave = "887788" }, x => x.Clave, x => x.Nombre);
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
