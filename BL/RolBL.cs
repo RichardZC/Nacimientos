5263a9b0362278102fcf5f9f47f87c60ae05e1f8
Rosario Alvarez <rosary_aar@outlook.com.pe>
2017-05-16 11:18:46 -0500

    mant persona

--- BL/RolBL.cs
+++ BL/RolBL.cs
@@ -36,5 +36,20 @@ namespace BL
                 return roles;
             }
         }
+
+        public static List<Roles> ListarRoles()
+        {
+            using (var bd = new nacEntities())
+            {
+                var roles = bd.rol.Select(x => new Roles()
+                {
+                    RolId = x.RolId,
+                    Denominacion = x.Denominacion,
+                    Estado = false
+                }).ToList();
+                
+                return roles;
+            }
+        }
     }
 }
---
+++ Web/Controllers/PersonaController.cs
@@ -0,0 +1,73 @@
+﻿using BE;
+using BL;
+using Comun;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Filters;
+
+
+namespace Web.Controllers
+{
+    [Autenticado]
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Buscar(string clave = "")
+        {
+            return PartialView(PersonaBL.ListarPersonas(clave.Trim()));
+        }
+
+        public ActionResult Mantener(int id = 0)
+        {
+            var per = new persona();
+            if (id == 0)
+            {
+                //var max = NacimientoBL.ObtenerUltimoLibro();
+                //nac.NroLibro = max[0];
+                //nac.NroActa = max[1] + 1;
+                //nac.Fecha = DateTime.Now;
+                //nac.Sexo = "M";
+            }
+            else
+                per = PersonaBL.Obtener(id);
+
+            return View(per);
+        }
+
+        [HttpPost]
+        public JsonResult Guardar(persona per)
+        {
+            var rm = new ResponseModel();
+            per.Nombres = per.Nombres.ToUpper();
+            per.Paterno = per.Paterno.ToUpper();
+            per.Materno = per.Materno.ToUpper();
+            per.NombreCompleto = per.Nombres + " " + per.Paterno + " " + per.Materno;
+
+            if (per.PersonaId == 0)
+            {
+                //nac.Url = string.Empty;
+                PersonaBL.Crear(per);
+
+            }
+            else
+            {
+
+                PersonaBL.Actualizar(per);
+            }
+
+            rm.SetResponse(true);
+            rm.function = "fn.notificar()";
+            rm.result = per.PersonaId.ToString();
+            
+            return Json(rm);
+        }
+    }
+}
