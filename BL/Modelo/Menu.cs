8cae5d59ccfb7a883be536ecc1b1230b18a3e9a1
Davidnsg <davidsgn@outlook.com>
2017-06-15 10:16:42 -0500

    seleccion menu

---
+++ BL/Modelo/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Modelo
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Denominacion { get; set; }
+        public string Modulo { get; set; }
+        public string Icono { get; set; }
+        public Nullable<bool> IndPadre { get; set; }
+        public Nullable<int> Orden { get; set; }
+        public Nullable<int> Referencia { get; set; }
+        public bool Estado { get; set; }
+    }
+}
