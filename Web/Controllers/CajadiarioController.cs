ac6dd09bfc41deecd362792fea03e4ea6597eaee
Davidnsg <davidsgn@outlook.com>
2017-06-15 10:25:01 -0500

    seleccionar menu

---
+++ Web/Controllers/CajadiarioController.cs
@@ -0,0 +1,18 @@
+﻿using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class CajadiarioController : Controller
+    {
+        // GET: Cajadiario
+        public ActionResult Index()
+        {
+            return View(CajadiarioBL.Listar(includeProperties: "caja"));
+        }
+    }
+}
